1dcdbd9849c1142579f532bbdfcb68f5aa6b68b2
Nemironium <nemiron@outlook.com>
2020-03-27 17:56:42 +0300

    Add: future shared ViewModel

---
+++ app/src/main/java/io/nemiron/meetgo/core/entities/UserInfo.kt
@@ -0,0 +1,8 @@
+package io.nemiron.meetgo.core.entities
+
+data class UserInfo (
+    var id: Int? = null,
+    var name: String? = null,
+    var email: String? = null,
+    var token: String? = null
+)
