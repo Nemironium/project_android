7ade441aae9ff8536ac29a823bd72be62c70d344
Nemironium <nemiron@outlook.com>
2020-05-17 17:12:58 +0300

    Add: ViewBinding delegate in Registration Issue: NMRN-T-39

--- app/src/main/java/io/nemiron/meetgo/view/login/registration/RegistrationFragment.kt
+++ app/src/main/java/io/nemiron/meetgo/view/login/registration/RegistrationFragment.kt
@@ -1,33 +1,26 @@
 package io.nemiron.meetgo.view.login.registration
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import io.nemiron.meetgo.R
 import io.nemiron.meetgo.core.helpers.AppPrefs
 import io.nemiron.meetgo.databinding.FragmentRegistrationBinding
 import org.koin.android.ext.android.inject
 
-class RegistrationFragment : Fragment() {
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
 
-    private lateinit var registrationBinding: FragmentRegistrationBinding
+    private val binding: FragmentRegistrationBinding by viewBinding()
     private val appPrefs: AppPrefs by inject()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        registrationBinding = FragmentRegistrationBinding.inflate(inflater, container, false)
-
-        registrationBinding.registerButton.setOnClickListener {
+        binding.registerButton.setOnClickListener {
             appPrefs.isLogged = true
             findNavController().navigate(R.id.action_registration_to_changePartner)
         }
-
-        return registrationBinding.root
     }
 }
