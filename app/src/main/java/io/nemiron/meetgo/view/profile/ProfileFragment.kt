e207688fc6052d68f498b36cfa95ade079f608b8
Nemironium <nemiron@outlook.com>
2020-05-17 17:14:37 +0300

    Add: ViewBinding delegate in Profile Issue: NMRN-T-39

--- app/src/main/java/io/nemiron/meetgo/view/profile/ProfileFragment.kt
+++ app/src/main/java/io/nemiron/meetgo/view/profile/ProfileFragment.kt
@@ -1,42 +1,33 @@
 package io.nemiron.meetgo.view.profile
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import io.nemiron.meetgo.R
+import io.nemiron.meetgo.databinding.FragmentProfileBinding
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
     private lateinit var profileViewModel: ProfileViewModel
+    private val binding: FragmentProfileBinding by viewBinding()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         profileViewModel =
             ViewModelProviders.of(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_profile)
+
         profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            binding.textProfile.text = it
         })
 
-        root.findViewById<Button>(R.id.button).setOnClickListener {
+        binding.button.setOnClickListener {
             findNavController().navigate(R.id.action_profile_to_settings)
         }
 
-        root.findViewById<Button>(R.id.button2).setOnClickListener {
-            findNavController().navigate(R.id.action_profile_to_changePartner)
-        }
-
-        return root
     }
 }
