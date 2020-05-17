6c93c69653723b197864f39939fcebe4a11fe906
Nemironium <nemiron@outlook.com>
2020-05-17 17:12:22 +0300

    Add: ViewBinding delegate in Home Issue: NMRN-T-39

--- app/src/main/java/io/nemiron/meetgo/view/home/HomeFragment.kt
+++ app/src/main/java/io/nemiron/meetgo/view/home/HomeFragment.kt
@@ -1,34 +1,27 @@
 package io.nemiron.meetgo.view.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import by.kirich1409.viewbindingdelegate.viewBinding
 import io.nemiron.meetgo.R
+import io.nemiron.meetgo.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var homeViewModel: HomeViewModel
+    private val binding: FragmentHomeBinding by viewBinding()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
 
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            binding.textHome.text = it
         })
-
-
-        return root
     }
 }
