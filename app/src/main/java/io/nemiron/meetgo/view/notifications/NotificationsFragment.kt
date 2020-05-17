cccaa22f800123239c4f6230b79d40b53b51b7c2
Nemironium <nemiron@outlook.com>
2020-05-17 17:13:53 +0300

    Add: ViewBinding delegate in Notifications Issue: NMRN-T-39

--- app/src/main/java/io/nemiron/meetgo/view/notifications/NotificationsFragment.kt
+++ app/src/main/java/io/nemiron/meetgo/view/notifications/NotificationsFragment.kt
@@ -1,31 +1,26 @@
 package io.nemiron.meetgo.view.notifications
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import by.kirich1409.viewbindingdelegate.viewBinding
 import io.nemiron.meetgo.R
+import io.nemiron.meetgo.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
 
+    private val binding: FragmentNotificationsBinding by viewBinding()
     private lateinit var notificationsViewModel: NotificationsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            binding.textNotifications.text = it
         })
-        return root
     }
 }
