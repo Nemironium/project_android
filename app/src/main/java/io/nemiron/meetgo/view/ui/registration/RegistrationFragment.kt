4834cb6dfcf66061ad0ae9beb266519fe7af6281
Nemironium <nemiron@outlook.com>
2020-05-25 23:22:33 +0300

    Update: error handling in registration fragment Issue: https://nemiron.jetbrains.space/p/nmrn/issues/27

--- app/src/main/java/io/nemiron/meetgo/view/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/io/nemiron/meetgo/view/ui/registration/RegistrationFragment.kt
@@ -1,10 +1,15 @@
 package io.nemiron.meetgo.view.ui.registration
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.textfield.TextInputLayout.END_ICON_CUSTOM
+import com.google.android.material.textfield.TextInputLayout.END_ICON_PASSWORD_TOGGLE
 import io.nemiron.meetgo.R
 import io.nemiron.meetgo.databinding.FragmentRegistrationBinding
 import io.nemiron.meetgo.helpers.AuthorizationHelper
@@ -17,9 +22,94 @@ class RegistrationFragment(private val authorizationHelper: AuthorizationHelper)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setGenderDropdownAdapter()
+        showIncorrectPasswordError()
+        showCorrectPassword()
+
         binding.registerButton.setOnClickListener {
-            //authorizationHelper.isLogged = true
+            /*authorizationHelper.sessionId = "test"
+            authorizationHelper.userId = "test"*/
             findNavController().navigate(R.id.action_registration_to_changePartner)
         }
+
+        /*
+        * Чтобы сделать нормальное показ ошибки для поля пароля.
+        * Нужно:
+        *       1. включить app:errorEnabled="true" (если не включать, то оно должно автоматом ставиться
+        *           и место под поледм должно расширяться) TODO(проверить это)
+        *       2. выставить ошибку passwordLayout.error = "error"
+        *       3. чтобы убрать ошибку, нужно сделать passwordLayout.error = null
+        * */
+    }
+
+    private fun setGenderDropdownAdapter() = with(binding.genderField) {
+        setAdapter(
+            ArrayAdapter(
+                context,
+                android.R.layout.simple_dropdown_item_1line,
+                resources.getStringArray(R.array.gender_array)
+            )
+        )
+    }
+
+    /*работает */
+    private fun showPasswordConfirmed() = with(binding.confirmPasswordLayout) {
+        endIconMode = END_ICON_CUSTOM
+        endIconDrawable = ContextCompat.getDrawable(context, R.drawable.ic_check_circle_accept_24dp)
+        endIconContentDescription = getString(R.string.check_password_icon)
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+    }
+
+    /*работает */
+    private fun showPasswordNotConfirmedError() = with(binding.confirmPasswordLayout) {
+        endIconMode = END_ICON_PASSWORD_TOGGLE
+        endIconContentDescription = null
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
+    }
+
+    /*работает */
+    private fun showIncorrectUsernameError() = with(binding.usernameLayout) {
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
+        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorError))
+    }
+
+    /*работает, но не тот цвет, когда нет данных */
+    private fun showCorrectUsername() = with(binding.usernameLayout) {
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+    }
+
+    /*работает */
+    private fun showIncorrectPasswordError() = with(binding.passwordLayout) {
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
+        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorError))
+    }
+
+    /*работает, но не тот цвет, когда нет данных */
+    private fun showCorrectPassword() = with(binding.passwordLayout) {
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_accept_input_box_stroke)!!)
+        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorAccept))
+    }
+
+    /* вызывать, когда в поле пароль 0 символов*/
+    private fun showDefaultPassword() = with(binding.passwordLayout) {
+        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+    }
+
+    private fun showExistingUsernameError() = with(binding.usernameLayout) {
+        binding.usernameLayout.error = getString(R.string.existing_username_error)
+    }
+
+    private fun cleanUsernameError() = with(binding.usernameLayout) {
+        binding.usernameLayout.error = null
+    }
+
+    private fun showExistingEmailError() = with(binding.emailLayout) {
+        binding.usernameLayout.error = getString(R.string.existing_email_error)
+    }
+
+    private fun cleanEmailError() = with(binding.emailLayout) {
+        binding.usernameLayout.error = null
     }
 }
