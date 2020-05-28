7d9cefae0dae60b85ec6f6de5e0642cd1961046c
Nemironium <nemiron@outlook.com>
2020-05-28 20:34:52 +0300

    Updated:completed validation for registration screen Issue: https://nemiron.jetbrains.space/p/nmrn/issues/46

--- app/src/main/java/io/nemiron/meetgo/view/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/io/nemiron/meetgo/view/ui/registration/RegistrationFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.textfield.TextInputLayout.END_ICON_CUSTOM
@@ -13,52 +15,35 @@ import com.google.android.material.textfield.TextInputLayout.END_ICON_PASSWORD_T
 import io.nemiron.meetgo.R
 import io.nemiron.meetgo.databinding.FragmentRegistrationBinding
 import io.nemiron.meetgo.extensions.clearFocus
-import io.nemiron.meetgo.network.AuthorizationHelper
+import io.nemiron.meetgo.extensions.hide
+import io.nemiron.meetgo.extensions.show
+import io.nemiron.meetgo.view.states.RegistrationScreenState
 import io.nemiron.meetgo.view.viewmodels.RegistrationViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-
-class RegistrationFragment(private val authorizationHelper: AuthorizationHelper) : Fragment(R.layout.fragment_registration) {
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
 
     private val binding: FragmentRegistrationBinding by viewBinding()
 
-    private val registrationViewModel: RegistrationViewModel by viewModel()
+    private val viewModel: RegistrationViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+        viewModel.state.observe(viewLifecycleOwner, Observer { handleState(it) })
+    }
+
+    private fun initViews() = with(binding) {
         setGenderDropdownAdapter()
-
-
-        /*binding.loginButton.setOnClickListener {
-            clearScreenFocus()
-            with(binding) {
-                val data = RegistrationInfo(
-                    firstNameField.text.toString(),
-                    lastNameField.text.toString(),
-                    Gender.getGender(genderField.text.toString()),
-                    emailField.text.toString(),
-                    usernameField.text.toString(),
-                    passwordField.text.toString(),
-                    confirmPasswordField.text.toString()
-                )
-                Timber.d(data.toString())
-            }
-        }*/
-
-        binding.registerButton.setOnClickListener {
-            /*authorizationHelper.sessionId = "test"
-            authorizationHelper.userId = "test"*/
-            findNavController().navigate(R.id.action_registration_to_changePartner)
-        }
-
-        /*
-        * Чтобы сделать нормальное показ ошибки для поля пароля.
-        * Нужно:
-        *       1. включить app:errorEnabled="true" (если не включать, то оно должно автоматом ставиться
-        *           и место под поледм должно расширяться) TODO(проверить это)
-        *       2. выставить ошибку passwordLayout.error = "error"
-        *       3. чтобы убрать ошибку, нужно сделать passwordLayout.error = null
-        * */
+        firstNameField.doAfterTextChanged { viewModel.firstName = it?.toString() ?: "" }
+        lastNameField.doAfterTextChanged { viewModel.lastName = it?.toString() ?: "" }
+        genderField.doAfterTextChanged { viewModel.gender = it?.toString() ?: "" }
+        usernameField.doAfterTextChanged { viewModel.username = it?.toString() ?: "" }
+        emailField.doAfterTextChanged { viewModel.email = it?.toString() ?: "" }
+        passwordField.doAfterTextChanged { viewModel.password = it?.toString() ?: "" }
+        confirmPasswordField.doAfterTextChanged { viewModel.confirmPassword = it?.toString() ?: "" }
+        registerButton.setOnClickListener { requireActivity().clearFocus(view); viewModel.submitRegistration() }
+        loginButton.setOnClickListener { findNavController().popBackStack() }
     }
 
     private fun setGenderDropdownAdapter() = with(binding.genderField) {
@@ -71,62 +56,91 @@ class RegistrationFragment(private val authorizationHelper: AuthorizationHelper)
         )
     }
 
-    /*работает */
-    private fun showPasswordConfirmed() = with(binding.confirmPasswordLayout) {
-        endIconMode = END_ICON_CUSTOM
-        endIconDrawable = ContextCompat.getDrawable(context, R.drawable.ic_check_circle_accept_24dp)
-        endIconContentDescription = getString(R.string.check_password_icon)
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+    private fun handleState(state: RegistrationScreenState?) {
+        if (state?.isSuccessRegistration == true) {
+            findNavController().navigate(R.id.action_registration_to_changePartner)
+            return
+        }
+        processUsernameError(state?.isUsernameError)
+        processUsernameHighlighting(state?.isUsernameHighlighted)
+        processEmailError(state?.isEmailError)
+        processEmailHighlighting(state?.isEmailHighlighted)
+        processPasswordField(state?.isPasswordHighlighted)
+        processConfirmPasswordField(state?.isPasswordConfirmed)
+        processProgressBar(state?.isProgressIndicated)
+        processRegisterButton(state?.isRegisterButtonEnabled)
     }
 
-    /*работает */
-    private fun showPasswordNotConfirmedError() = with(binding.confirmPasswordLayout) {
-        endIconMode = END_ICON_PASSWORD_TOGGLE
-        endIconContentDescription = null
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
+    private fun processUsernameError(mode: Boolean?) {
+        binding.usernameLayout.error = if (mode == true)
+            getString(R.string.existing_username_error)
+        else
+            null
     }
 
-    /*работает */
-    private fun showIncorrectUsernameError() = with(binding.usernameLayout) {
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
-        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorError))
+    private fun processUsernameHighlighting(mode: Boolean?) = with(binding.usernameLayout) {
+        if (mode == true) {
+            isCounterEnabled = true
+            defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorError))
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
+        } else {
+            isCounterEnabled = false
+            defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+        }
     }
 
-    /*работает, но не тот цвет, когда нет данных */
-    private fun showCorrectUsername() = with(binding.usernameLayout) {
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
-        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+    private fun processEmailError(mode: Boolean?) {
+        binding.emailLayout.error = if (mode == true)
+            getString(R.string.existing_username_error)
+        else
+            null
     }
 
-    /*работает */
-    private fun showIncorrectPasswordError() = with(binding.passwordLayout) {
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
-        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorError))
+    private fun processEmailHighlighting(mode: Boolean?) = with(binding.emailLayout) {
+        defaultHintTextColor = if (mode == true) {
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_invalid_input_box_stroke)!!)
+            ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorError))
+        } else {
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+            ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+        }
     }
 
-    /*работает, но не тот цвет, когда нет данных */
-    private fun showCorrectPassword() = with(binding.passwordLayout) {
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_accept_input_box_stroke)!!)
-        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorAccept))
+    private fun processPasswordField(mode: Boolean?) = with(binding.passwordLayout) {
+        if (mode == true) {
+            isCounterEnabled = false
+            defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorAccept))
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_accept_input_box_stroke)!!)
+        } else {
+            isCounterEnabled = true
+            defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+        }
     }
 
-    /* вызывать, когда в поле пароль 0 символов*/
-    private fun showDefaultPassword() = with(binding.passwordLayout) {
-        setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
-        defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+    private fun processConfirmPasswordField(mode: Boolean?) = with(binding.confirmPasswordLayout) {
+        if (mode == true) {
+            endIconMode = END_ICON_CUSTOM
+            endIconDrawable = ContextCompat.getDrawable(context, R.drawable.ic_check_circle_accept_24dp)
+            defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorAccept))
+            setEndIconContentDescription(R.string.check_password_icon)
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_accept_input_box_stroke)!!)
+        } else {
+            endIconMode = END_ICON_PASSWORD_TOGGLE
+            defaultHintTextColor = ColorStateList.valueOf(ContextCompat.getColor(context, R.color.colorPrimary))
+            setBoxStrokeColorStateList(ContextCompat.getColorStateList(context, R.color.text_default_input_box_stroke)!!)
+        }
     }
 
-    private fun showExistingUsernameError()   {
-        binding.usernameLayout.error = getString(R.string.existing_username_error)
+    private fun processProgressBar(mode: Boolean?) = with(binding.registrationProgressBar) {
+        if (mode == true)
+            show()
+        else
+            hide()
     }
 
-    private fun cleanUsernameError() { binding.usernameLayout.error = null }
-
-    private fun showExistingEmailError() {
-        binding.usernameLayout.error = getString(R.string.existing_email_error)
+    private fun processRegisterButton(mode: Boolean?) {
+        binding.registerButton.isEnabled = mode == true
     }
-
-    private fun cleanEmailError() { binding.usernameLayout.error = null }
-
-    private fun clearScreenFocus() = requireActivity().clearFocus(view)
 }
--- app/src/main/java/io/nemiron/meetgo/view/viewmodels/RegistrationViewModel.kt
+++ app/src/main/java/io/nemiron/meetgo/view/viewmodels/RegistrationViewModel.kt
@@ -1,17 +1,126 @@
 package io.nemiron.meetgo.view.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.nemiron.domain.entities.Gender
+import io.nemiron.domain.entities.RegistrationInfo
+import io.nemiron.meetgo.view.states.RegistrationScreenState
 import io.nemiron.usecase.RegisterUserUseCase
 import io.nemiron.usecase.ValidateRegistrationDataUseCase
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RegistrationViewModel(
-    private val validateRegistrationDataUseCase: ValidateRegistrationDataUseCase,
+    private val validateUseCase: ValidateRegistrationDataUseCase,
     private val registerUserUseCase: RegisterUserUseCase
-) : CoroutineViewModel() {
+) : ViewModel() {
 
-    init {
-        viewModelScope
+    private val _state = MutableLiveData<RegistrationScreenState>()
+
+    val state: LiveData<RegistrationScreenState>
+        get() = _state
+
+    var firstName = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+    var lastName = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+    var gender = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+    var username = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+    var email = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+    var password = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+    var confirmPassword = ""
+        set(value) {
+            field = value
+            validateForm()
+        }
+
+/*
+    fun setUsernameError(`val`: Boolean) { _state.value?.isUsernameError = `val` }
+
+    fun setUsernameHighlighted(`val`: Boolean) { _state.value?.isUsernameHighlighted = `val` }
+
+    fun setEmailError(`val`: Boolean) { _state.value?.isEmailError = `val` }
+
+    fun setEmailHighlighted(`val`: Boolean) { _state.value?.isEmailHighlighted = `val` }
+
+    fun setPasswordHighlighted(`val`: Boolean) { _state.value?.isPasswordHighlighted = `val` }
+
+    fun setPasswordConfirmed(`val`: Boolean) { _state.value?.isPasswordConfirmed = `val` }
+
+    fun setProgressIndicator(`val`: Boolean) { _state.value?.isProgressIndicated = `val` }
+
+    fun setRegisterButtonEnabled(`val`: Boolean) { _state.value?.isRegisterButtonEnabled = `val` }
+
+    fun setRegistrationSuccess(`val`: Boolean) { _state.value?.isSuccessRegistration = `val` }
+*/
+
+    fun submitRegistration() {
+        viewModelScope.launch {
+            registerUserUseCase(RegistrationInfo(
+                firstName,
+                lastName,
+                Gender.getGender(gender),
+                username,
+                email,
+                password,
+                confirmPassword
+            ))
+        }
     }
 
+    private fun validateForm() {
+        val validation = validateUseCase(RegistrationInfo(
+            firstName,
+            lastName,
+            Gender.getGender(gender),
+            username,
+            email,
+            password,
+            confirmPassword
+        ))
 
+        Timber.d("validateUseCase: $validation")
+        Timber.d("validation.isAllValid: ${validation.isAllValid}")
+
+        _state.postValue(RegistrationScreenState(
+            isUsernameHighlighted = !validation.isUsernameCorrect,
+            isEmailHighlighted = !validation.isEmailCorrect,
+            isPasswordHighlighted = validation.isPasswordStrong,
+            isPasswordConfirmed = validation.isPasswordConfirmed,
+            isRegisterButtonEnabled = validation.isAllValid
+        ))
+
+        Timber.d("RegistrationScreenState: ${_state.value}")
+
+    }
 }
