60cb330031776fd67c76448edd70705f8fe7ef83
Nemironium <nemiron@outlook.com>
2020-05-28 22:58:43 +0300

    Refactoring Issue: https://nemiron.jetbrains.space/p/nmrn/issues/46

--- app/src/main/java/io/nemiron/meetgo/view/viewmodels/RegistrationViewModel.kt
+++ app/src/main/java/io/nemiron/meetgo/view/viewmodels/RegistrationViewModel.kt
@@ -10,7 +10,6 @@ import io.nemiron.meetgo.view.states.RegistrationScreenState
 import io.nemiron.usecase.RegisterUserUseCase
 import io.nemiron.usecase.ValidateRegistrationDataUseCase
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 class RegistrationViewModel(
     private val validateUseCase: ValidateRegistrationDataUseCase,
@@ -64,37 +63,19 @@ class RegistrationViewModel(
             validateForm()
         }
 
-/*
-    fun setUsernameError(`val`: Boolean) { _state.value?.isUsernameError = `val` }
-
-    fun setUsernameHighlighted(`val`: Boolean) { _state.value?.isUsernameHighlighted = `val` }
-
-    fun setEmailError(`val`: Boolean) { _state.value?.isEmailError = `val` }
-
-    fun setEmailHighlighted(`val`: Boolean) { _state.value?.isEmailHighlighted = `val` }
-
-    fun setPasswordHighlighted(`val`: Boolean) { _state.value?.isPasswordHighlighted = `val` }
-
-    fun setPasswordConfirmed(`val`: Boolean) { _state.value?.isPasswordConfirmed = `val` }
-
-    fun setProgressIndicator(`val`: Boolean) { _state.value?.isProgressIndicated = `val` }
-
-    fun setRegisterButtonEnabled(`val`: Boolean) { _state.value?.isRegisterButtonEnabled = `val` }
-
-    fun setRegistrationSuccess(`val`: Boolean) { _state.value?.isSuccessRegistration = `val` }
-*/
-
     fun submitRegistration() {
         viewModelScope.launch {
-            registerUserUseCase(RegistrationInfo(
-                firstName,
-                lastName,
-                Gender.getGender(gender),
-                username,
-                email,
-                password,
-                confirmPassword
-            ))
+            registerUserUseCase(
+                RegistrationInfo(
+                    firstName,
+                    lastName,
+                    Gender.getGender(gender),
+                    username,
+                    email,
+                    password,
+                    confirmPassword
+                )
+            )
         }
     }
 
@@ -109,9 +90,6 @@ class RegistrationViewModel(
             confirmPassword
         ))
 
-        Timber.d("validateUseCase: $validation")
-        Timber.d("validation.isAllValid: ${validation.isAllValid}")
-
         _state.postValue(RegistrationScreenState(
             isUsernameHighlighted = !validation.isUsernameCorrect,
             isEmailHighlighted = !validation.isEmailCorrect,
@@ -119,8 +97,5 @@ class RegistrationViewModel(
             isPasswordConfirmed = validation.isPasswordConfirmed,
             isRegisterButtonEnabled = validation.isAllValid
         ))
-
-        Timber.d("RegistrationScreenState: ${_state.value}")
-
     }
 }
